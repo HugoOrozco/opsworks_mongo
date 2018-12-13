c26536b16c92cbe4931ddcf1aeb135ded6658f8d
Hugo Orozco <hugo@nclouds.com>
2018-12-13 16:10:48 -0600

    running create admin user on every node

--- mongodb3/recipes/users.rb
+++ mongodb3/recipes/users.rb
@@ -6,19 +6,9 @@ require 'aws-sdk-opsworks'
 ::Chef::Recipe.send(:include, MongoDB::Helpers::User)
 
 # Obtaning mongo instnaces
-this_instance = search("aws_opsworks_instance", "self:true").first
-layer_id = this_instance["layer_ids"][0]
-
-opsworks = Aws::OpsWorks::Client.new(:region => "#{node['Region']}")
 
 ruby_block 'Adding Admin User' do
     block do
-        master_node_command = opsworks.describe_instances({
-            layer_id: layer_id,
-        })
-        master_node = master_node_command.instances[0].hostname
-        if master_node == this_instance["hostname"]
-            create_admin_user()
-        end
+        create_admin_user()
     end
 end
