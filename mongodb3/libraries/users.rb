6e03acf3680af58849c75c424808f85b4f4c776e
Hugo Orozco <hugo@nclouds.com>
2018-12-13 16:48:24 -0600

    bug fixes

--- mongodb3/libraries/users.rb
+++ mongodb3/libraries/users.rb
@@ -1,75 +1,70 @@
-module MongoDB
-    module Helpers
-        module User
+module UserHelper
+    def user_exists?(username, connection)
+        connection['system.users'].find(user: username).count > 0
+    end
 
-            def user_exists?(username, connection)
-                connection['system.users'].find(user: username).count > 0
+    def create_admin_user()
+        require 'mongo'
+        begin
+            username = node['DBUser']
+            password = node['DBPass']
+
+            begin
+                client = Mongo::Client.new([ '127.0.0.1:27017' ],:database => "admin" ,:connect => "direct", :server_selection_timeout => 5)
+                client.database_names
+            rescue
+                client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => "admin", :user => username, :password => password,:connect => "direct", :server_selection_timeout => 5)
             end
+        rescue
+            Chef::Log.info "Failed to connect to database"
+        end
 
-            def create_admin_user()
-                require 'mongo'
-                begin
-                    username = node['DBUser']
-                    password = node['DBPass']
+        db = client.use('admin')
+        roles = ['role': 'userAdminAnyDatabase', 'db': 'admin']
+        create_user(username, password, roles, db, client)
 
-                    begin
-                        client = Mongo::Client.new([ '127.0.0.1:27017' ],:database => "admin" ,:connect => "direct", :server_selection_timeout => 5)
-                        client.database_names
-                    rescue
-                        client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => "admin", :user => username, :password => password,:connect => "direct", :server_selection_timeout => 5)
-                    end
-                rescue
-                    Chef::Log.info "Failed to connect to database"
-                end
+    end
 
-                db = client.use('admin')
-                roles = ['role': 'userAdminAnyDatabase', 'db': 'admin']
-                create_user(username, password, roles, db, client)
-
+    def create_user(username, password, roles, db, client)
+        begin
+            if !user_exists?(username, client)
+                db.database.users.create(
+                            username,
+                            password: password,
+                            roles: roles
+                            )
+            else
+                Chef::Log.info "User " + username + " already exists"
             end
+        rescue
+            Chef::Log.info "Could not create user: " + username
+        end
 
-            def create_user(username, password, roles, db, client)
-                begin
-                    if !user_exists?(username, client)
-                        db.database.users.create(
-                                    username,
-                                    password: password,
-                                    roles: roles
-                                    )
-                    else
-                        Chef::Log.info "User " + username + " already exists"
-                    end
-                rescue
-                    Chef::Log.info "Could not create user: " + username
-                end
+    end
 
-            end
+    def create_all_users()
+        require 'mongo'
 
-            def create_all_users()
-                require 'mongo'
+        users = []
+        roles = ['role': 'userAdminAnyDatabase', 'db': 'admin']
+        user = [{'username': 'User1', 'password': 'test', 'roles': roles}]
+        users.push(user)
+        user2 = [{'username': 'User2', 'password': 'test', 'roles': roles}]
+        users.push(user2)
 
-                users = []
-                roles = ['role': 'userAdminAnyDatabase', 'db': 'admin']
-                user = [{'username': 'User1', 'password': 'test', 'roles': roles}]
-                users.push(user)
-                user2 = [{'username': 'User2', 'password': 'test', 'roles': roles}]
-                users.push(user2)
+        begin
+            client = Mongo::Client.new([ '127.0.0.1:27017' ],:database => "admin" ,:connect => "direct", :server_selection_timeout => 5)
+            client.database_names
+        rescue
+            client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => "admin", :user => username, :password => password,:connect => "direct", :server_selection_timeout => 5)
+        end
+        
+        db = client.use('admin')
 
-                begin
-                    client = Mongo::Client.new([ '127.0.0.1:27017' ],:database => "admin" ,:connect => "direct", :server_selection_timeout => 5)
-                    client.database_names
-                rescue
-                    client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => "admin", :user => username, :password => password,:connect => "direct", :server_selection_timeout => 5)
-                end
-                
-                db = client.use('admin')
-
-                users.each do |user|
-                    create_user(user[:username], user[:password], user[:roles], db, client)
-                end
-            end
+        users.each do |user|
+            create_user(user[:username], user[:password], user[:roles], db, client)
         end
     end
 end
 
-Chef::Recipe.include(MongoDB::Helpers::User)
+#Chef::Recipe.include(MongoDB::Helpers::User)
--- mongodb3/recipes/users.rb
+++ mongodb3/recipes/users.rb
@@ -3,12 +3,14 @@ include_recipe "mongodb3::mongo_gem"
 require 'mongo'
 require 'aws-sdk-opsworks'
 
+include UserHelper
+
 ::Chef::Recipe.send(:include, MongoDB::Helpers::User)
 
 # Obtaning mongo instnaces
 
 ruby_block 'Adding Admin User' do
     block do
-        create_admin_user()
+        UserHelper.create_admin_user()
     end
 end
